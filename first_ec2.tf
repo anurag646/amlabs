9a97c6c48271a1b1ad1d3ec8a6b04db25da41505
anurag646 <anurag.mittal646@gmail.com>
2024-10-27 20:14:48 +0530

    Update first_ec2.tf

--- first_ec2.tf
+++ first_ec2.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "myec" {
     ami = "ami-06b21ccaeff8cd686"
     instance_type = "t2.micro"
-    count = 3
+    count = 4
     tags = {
       Name = "Bastion-${count.index}"
     }
