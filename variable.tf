62364fcd1fa2b16b984e9e100bdf3bb6f62f27dd
anurag646 <anurag.mittal646@gmail.com>
2024-10-27 20:00:22 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,4 @@
 variable "Key" {
-  access_key = "AKIAWL5MVQJUYSG5MKAD"
-  secret_key = "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"
+  key = "AKIAWL5MVQJUYSG5MKAD"
+  value = "ne5iR5yuPjgQX3pEiwwHUV3EwPe6e0U4dLZ0iCVy"
 }
