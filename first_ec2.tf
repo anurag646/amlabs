a7402263d6e6f312a3e2cbe8b4b52465968188d6
anurag646 <anurag.mittal646@gmail.com>
2024-10-27 16:12:54 +0530

    2

--- first_ec2.tf
+++ first_ec2.tf
@@ -4,8 +4,10 @@ provider "aws" {
   secret_key = "ne5iR5yuPjgQX3pEiwwHUV3EwPe6e0U4dLZ0iCVy"
 }
 
-resource "aws_instance" "myec222" {
+resource "aws_instance" "myec" {
     ami = "ami-06b21ccaeff8cd686"
     instance_type = "t2.micro"
     count = 3
+    tags = {
+      Name = "Bastion-${count.index}"
 }
